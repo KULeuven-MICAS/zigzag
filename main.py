b7d4a21d897c16e49cdc2059edf0ffa3e3c96ebd
asyms <asymons@micaszb03.esat.kuleuven.be>
2022-06-23 10:58:58 +0200

    Add comments to main.py

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from classes.stages import *
 import argparse
+
+# Parse the workload and accelerator arguments
 parser = argparse.ArgumentParser(description="Setup zigzag-v2 inputs")
 parser.add_argument('--workload', metavar='path', required=True, help='module path to workload, e.g. inputs.examples.workload1')
 parser.add_argument('--accelerator', metavar='path', required=True, help='module path to the accelerator, e.g. inputs.examples.accelerator1')
-
 args = parser.parse_args()
+
+# Initialize the logger
 import logging as _logging
 _logging_level = _logging.INFO
 _logging_format = '%(asctime)s - %(name)s.%(funcName)s +%(lineno)s - %(levelname)s - %(message)s'
 _logging.basicConfig(level=_logging_level,
                      format=_logging_format)
 
+# Initialize the MainStage which will start execution.
+# The first argument of this init is the list of stages that will be executed in sequence.
+# The second argument of this init are the arguments required for these different stages.
 mainstage = MainStage([
     WorkloadAndAcceleratorParserStage,
     CompleteSaveStage,
@@ -26,4 +32,6 @@ mainstage = MainStage([
     plot_filename_pattern="outputs_workshop/temporal_mappings.png",
     loma_lpf_limit=6
 )
+
+# Launch the MainStage
 mainstage.run()
