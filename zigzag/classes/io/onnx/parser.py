dfbf9f5a655ae89e0b6ef0d694c82cde2da94d33
asyms <arne.symons@kuleuven.be>
2023-02-02 18:09:29 +0100

    fix spatial mapping parsing for onnx nodes

--- zigzag/classes/io/onnx/parser.py
+++ zigzag/classes/io/onnx/parser.py
@@ -16,3 +16,16 @@ class Parser(metaclass=ABCMeta):
     @abstractmethod
     def run(self):
         ...
+
+    @staticmethod
+    def get_spatial_mappings(accelerator, core_allocation):
+        # If there is only one possible core allocation, set the spatial mapping as the one(s) of that core
+        if isinstance(core_allocation, int):
+            core = accelerator.get_core(core_allocation)
+            spatial_mappings = core.dataflows
+        elif (isinstance(core_allocation, list) and len(core_allocation) == 1):
+            core = accelerator.get_core(core_allocation[0])
+            spatial_mappings = core.dataflows
+        else:
+            spatial_mappings = None
+        return spatial_mappings
