bbe197e377db3a1a48b05d6d392315ca88006928
asyms <arne.symons@kuleuven.be>
2023-04-19 14:54:44 +0200

    remove upper level inputs folder; inputs should be under zigzag/

--- inputs/examples/mapping/default.py
+++
@@ -1,7 +0,0 @@
-mapping = {
-    "default": {
-        "core_allocation": 1,
-        "spatial_mapping": {'D1': ('K', 32), 'D2': ('C', 32)},
-        "memory_operand_links": {'O': 'O', 'W': 'I2', 'I': 'I1'}
-    }
-}
