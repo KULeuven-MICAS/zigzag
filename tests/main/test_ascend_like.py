f3d3a092961d7e9406acaca13fb643691db36100
asyms <arne.symons@kuleuven.be>
2023-10-02 17:28:41 +0200

    add ascend_like tests

--- tests/main/test_ascend_like.py
+++ tests/main/test_ascend_like.py
@@ -9,6 +9,14 @@ workloads = (
     "zigzag.inputs.examples.workload.resnet18",
 )
 
+# Expected energy and latency for each workload defined above
+ens_lats = {
+    "zigzag/inputs/examples/workload/alexnet.onnx": (5738192980.375, 8728331),
+    "zigzag/inputs/examples/workload/mobilenetv2.onnx": (1913797698.5250015, 7426499),
+    "zigzag/inputs/examples/workload/resnet18.onnx": (1858697886.165, 3720129),
+    "zigzag.inputs.examples.workload.resnet18": (2408671233.7250004, 4804196),
+}
+
 
 @pytest.fixture
 def mapping():
@@ -22,5 +30,11 @@ def accelerator():
 
 @pytest.mark.parametrize("workload", workloads)
 def test_api(workload, accelerator, mapping):
-    answer = get_hardware_performance_zigzag(workload, accelerator, mapping)
-    assert answer
+    (energy, latency, cmes) = get_hardware_performance_zigzag(
+        workload, accelerator, mapping
+    )
+    print(f"{energy=}, {latency=}")
+    (expected_energy, expected_latency) = ens_lats[workload]
+    print(f"{expected_energy=}, {expected_latency=}")
+    assert energy == pytest.approx(expected_energy)
+    assert latency == pytest.approx(expected_latency)
