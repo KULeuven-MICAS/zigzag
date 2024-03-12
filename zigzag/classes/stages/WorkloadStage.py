1a1a72f76971b4505629de2d4f1b7a7c463e3fd8
JiacongSun <jiacong.sun@kuleuven.be>
2024-03-12 17:57:55 +0100

    Skip Add and Pooling layers for imc cores.

--- zigzag/classes/stages/WorkloadStage.py
+++ zigzag/classes/stages/WorkloadStage.py
@@ -12,16 +12,32 @@ class WorkloadStage(Stage):
 
     ## The class constructor
     # Initialization of self.workload.
-    def __init__(self, list_of_callables, *, workload, **kwargs):
+    def __init__(self, list_of_callables, *, workload, accelerator, **kwargs):
         super().__init__(list_of_callables, **kwargs)
         self.workload = workload
+        self.accelerator = accelerator
 
     def run(self):
         for id, layer in enumerate(nx.topological_sort(self.workload)):
             if type(layer) == DummyNode:
                 continue  # skip the DummyNodes
+            # Skip a layer if the layer type is "Pooling" and the hardware template is an IMC core.
+            # This wil have impact when the workload is defined manually.
+            # If the workload is from onnx, no skipping will be done.
+            core_id = layer.core_allocation
+            core = self.accelerator.get_core(core_id)
+            operational_array = core.operational_array
+            pe_type = getattr(operational_array, "pe_type", None)  # return None if it does not exist
+            try:  # branch if the workload is manually defined
+                layer_type = layer.layer_attrs["operator_type"]
+            except KeyError:  # branch if the workload is from an onnx (key "operator_type" does not exist)
+                layer_type = None
+            if (pe_type in ["in_sram_computing"]) and (layer_type in ["Pooling", "Add"]):
+                continue
+
             kwargs = self.kwargs.copy()
             kwargs["layer"] = layer
+            kwargs["accelerator"] = self.accelerator
             if layer.name:
                 layer_name = layer.name
             else:
