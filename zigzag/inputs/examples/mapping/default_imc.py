18fb46d532ea4ec9f116b399664100a6ff0e5989
JiacongSun <jiacong.sun@kuleuven.be>
2024-03-12 17:58:52 +0100

    Add mapping definition for Add layers to avoid errors in SearchUnusedMemoryStage when the workload is manually defined.

--- zigzag/inputs/examples/mapping/default_imc.py
+++ zigzag/inputs/examples/mapping/default_imc.py
@@ -4,5 +4,10 @@ mapping = {
         # "spatial_mapping": {"D1": ("OX", 25), "D2": (("FX", 3), ("FY", 3))},
         "memory_operand_links": {"O": "O", "W": "I2", "I": "I1"},
         "spatial_mapping_hint": {"D1": ["K", "OX"], "D2": ["C", "FX", "FY"]},
-    }
+    },
+    "Add": {  # to avoid errors when the workload is manually defined and contains Add layers.
+        "core_allocation": 1,
+        "memory_operand_links": {"O": "O", "X": "I2", "Y": "I1"},
+        "spatial_mapping_hint": {"D1": ["G"], "D2": ["C"]},
+        },
 }
