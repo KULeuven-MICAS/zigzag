02e98631e17c524a7625b0c5e7175789786f576b
jorendumoulin <joren.dumoulin@kuleuven.be>
2024-05-08 16:49:47 +0200

    use sphinx docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ release = "2.0.0"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosummary"]
 
 # extensions = [
 #     'sphinx.ext.autodoc',
@@ -47,7 +47,6 @@ templates_path = ["_templates"]
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
-
 
 # -- Options for HTML output -------------------------------------------------
 
