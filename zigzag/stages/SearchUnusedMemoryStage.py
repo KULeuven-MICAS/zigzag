ada2d109596d395a9dc6bceed7c79da5cc989f50
JiacongSun <jiacong.sun@kuleuven.be>
2024-05-20 17:14:49 +0200

    clean big comment block

--- zigzag/stages/SearchUnusedMemoryStage.py
+++ zigzag/stages/SearchUnusedMemoryStage.py
@@ -13,89 +13,15 @@ from zigzag.workload.DummyNode import DummyNode
 import logging
 
 from zigzag.workload.layer_node import LayerNode
+from zigzag.workload.LayerNodeABC import LayerNodeABC
 
 logger = logging.getLogger(__name__)
-
-# ########## Description ##########
-# TODO clean up. Don't think anyone will read this...
-# # This stage must be processed before WorkloadStage.
-# # This stage figures out the unused memory levels for "I", "W", "O" when the size of lower memory
-# # level is enough to hold all data, considering the output data of previous layer can be directly
-# # used by next layer. As an impact, the energy / latency related to these memories will be removed.
-# # The general criteria is:
-# #      If a low-level memory size is big enough to hold both "I" and "O" data of current layer,
-# #      memory above this one will be labeled as unused.
-# #      If a low-level memory size is big enough to hold "W" data of entire workload, memory above
-# #      this one will be labeled as unused.
-# # The above method only applies layers along the same branch, otherwise (for branch starting nodes
-# # or branch final nodes) the "O" data will return back to the top possible memory.
-# # In RemoveNoUseMemStage, unused mem across all layers, labeled in this stage, will be removed in
-# # the memory architecture.
-# # For now, the number of cores must be 1.
-# ########## Pseudo-code ##########
-# # Initialization:
-# #   mem_update_list = [layer_ids: {"I" / "O": -1}] # mem level of different operands of each layer
-# #   (there should be no -1 after self.update_top_mem_level())
-# #   each_layer_IO_data_size = [layer_ids: {"I" / "O": size}] # input / output data size of each layer
-# #   mem_update_weight = top_mem_level # top mem level to put weight
-# #   weight_size_entire_workload = weight_size # weight data size of entire workload
-# # Generate:
-# #   layer_execution_order = list( topological_sort(layer_graph) )
-# # Locate top mem level for each operand of each layer. Store results in mem_update_list and mem_update_weight.
-# #   for layer in all_layers:
-# #     if layer.index != 0: # not the 1st execution layer
-# #       mem_update_list[layer]["I"] = mem_update_list[previous_layer]["O"]
-# #     if len(layer.next_node) > 1 or len(next_layer.previous_node) > 1: # starting node of branches / final node of branches
-# #     | if layer.index == 0:
-# #     |   mem_update_list[layer]["I" / "O"] updates to the top input/output mem level
-# #     | else:
-# #     |   mem_update_list[layer]["O"] updates to the top output mem level
-# #     |   mem_update_weight = top weight mem level, if mem_update_weight > top weight mem level
-# #     |
-# #     else:
-# #       for mem in mem_levels(sort_order: from top to bottom):
-# #         if sum(layer[operand_size] for operand in mem.operands) <= mem.size:
-# #           if ["I", "O"] both in mem.operands:
-# #             mem_update_list[layer]["O"] = current_mem_level
-# #             if layer.index == 0: # the 1st execution layer
-# #               mem_update_list[layer]["I"] = current_mem_level
-# #           if ("W" in mem.operand) and (current_mem_level < mem_update_weight):
-# #             mem_update_weight = current_mem_level
-# ##########################
-#  Special note for Adder layers:
-#   Currently the algorithm is tricky for Adder layers. As for a conv/pool layer, required I, O
-#   sizes are put in each_layer_IO_data_size and the weight data size will be accumulated in
-#   weight_size_entire_workload.
-#   But for Adder layers, (1) there is no weight operand (or constant operand); (2) there are two
-#   input operands. (3) the info regarding which of the two operands is represented as I1 or I2 is
-#   not saved in self.workload, though it is defined in the input file.
-#   So, the current solution is:
-#   (1) for weight, the data amount is 0, which means weight_size_entire_workload will not consider
-#   Adder layers. (2) for act, we add up the data size of the two (or multiple) inputs and treat the
-#   sum as the act data size for the current layer, which is stored in each_layer_IO_data_size.
-#   What does this mean?
-#   This means for Adder layers, the required act data size is over-estimated, because we also
-#   include the data amount of the other operand, which we may have defined separate mem for the
-#   other operand. In other words, for a mem level with enough size to hold both O, I1 (assume I1 is
-#   the mem representation for one input), may be thought by the code that the size is not enough
-#   and therefore the output cannot be stored at this level.
-#   But keep in mind that!!!!!:
-#   This is only a problem when you use manually-defined workload and there are Adder layers.
-#   There is no problem if your workload is an .onnx file, because Adder layers will be skipped by
-#   default.
-#   Is there a solution?
-#   The reason why it cannot be fixed is we do not know which operand is from which layer.
-#   This problem can be fixed unless this info granularity is saved in the self.workload object,
-#   which is a networkx graph.
 
 
 class SearchUnusedMemoryStage(Stage):
     def __init__(
         self, list_of_callables: list[StageCallable], *, accelerator: Accelerator, workload: Workload, **kwargs: Any
     ):
-        """
-        # TODO needs cleanup
-        """
         super().__init__(list_of_callables, **kwargs)
         self.accelerator = accelerator
         self.workload = workload
@@ -113,7 +39,7 @@ class SearchUnusedMemoryStage(Stage):
         # record of the weight size of entire workload (unit: bit)
         self.weight_size_entire_workload: int = 0
         # record of executable layer name and index: dict[LayerNode, layer_idx]
-        self.layer_list: dict[LayerNode, int] = {}
+        self.layer_list: dict[LayerNodeABC, int] = {}
 
         # derive input, output size per layer and weight size of entire workload
         for idx, layer in enumerate(workload.topological_sort()):
