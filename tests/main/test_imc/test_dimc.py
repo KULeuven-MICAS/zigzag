b5b8d535627e88a42e2fd5d480d6720b69114296
jiacong sun <jiacong.sun@kuleuven.be>
2024-05-22 00:55:34 +0200

    update perf number

--- tests/main/test_imc/test_dimc.py
+++ tests/main/test_imc/test_dimc.py
@@ -12,7 +12,7 @@ workloads = (
 # Expected energy, latency (#cycles), clk time and area for each workload defined above
 ens_lats_clks_areas = {
     "inputs/workload/alexnet.onnx": (6812722722.515776, 9460061.0, 3.75708, 0.8566212024),
-    "inputs/workload/mobilenetv2.onnx": (2857471628.5898623, 20501552.0, 3.75708, 0.8566212024),
+    "inputs/workload/mobilenetv2.onnx": (2884947078.1898627, 20454032.0, 3.75708, 0.8566212024),
     "inputs/workload/resnet18.onnx": (4726270705.225856, 6337852.0, 3.75708, 0.8566212024),
     "inputs/workload/resnet18.yaml": (4268432908.954752, 5789353.0, 3.75708, 0.8566212024),
 }
@@ -25,7 +25,7 @@ def mapping():
 
 @pytest.fixture
 def accelerator():
-    return "inputs/hardware/dimc.yaml"
+    return "inputs/hardware/aimc.yaml"
 
 
 @pytest.mark.parametrize("workload", workloads)
