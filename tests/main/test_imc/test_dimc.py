7d208bd6d1a4101ab2fcdd31945b6013eb3c9e4d
jiacong sun <jiacong.sun@kuleuven.be>
2024-05-21 22:47:37 +0200

    update perf number

--- tests/main/test_imc/test_dimc.py
+++ tests/main/test_imc/test_dimc.py
@@ -11,10 +11,10 @@ workloads = (
 
 # Expected energy, latency (#cycles), clk time and area for each workload defined above
 ens_lats_clks_areas = {
-    "inputs/workload/alexnet.onnx": (2340181787.2719307, 72692592.0, 3.2026, 0.785592664),
-    "inputs/workload/mobilenetv2.onnx": (703506891.3687075, 28005964.0, 3.2026, 0.785592664),
+    "inputs/workload/alexnet.onnx": (6812722722.515776, 9460061.0, 3.75708, 0.8566212024),
+    "inputs/workload/mobilenetv2.onnx": (2857471628.5898623, 20501552.0, 3.75708, 0.8566212024),
     "inputs/workload/resnet18.onnx": (1828766840.9463186, 120700590.0, 3.2026, 0.785592664),
-    "inputs/workload/resnet18.yaml": (2008581031.8287854, 130747736.0, 3.2026, 0.785592664),
+    "inputs/workload/resnet18.yaml": (4268432908.954752, 5789353.0, 3.75708, 0.8566212024),
 }
 
 
@@ -29,7 +29,7 @@ def accelerator():
 
 
 @pytest.mark.parametrize("workload", workloads)
-def test_api(workload, accelerator, mapping):
+def test_api(workload: str, accelerator: str, mapping: str):
     (energy, latency, tclk, area, cmes) = get_hardware_performance_zigzag_imc(workload, accelerator, mapping)
     (expected_energy, expected_latency, expected_tclk, expected_area) = ens_lats_clks_areas[workload]
     assert energy == pytest.approx(expected_energy)
