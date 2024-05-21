d571fdfc2a6705e1031d23e8eb6bc3443e03a1b0
jiacong sun <jiacong.sun@kuleuven.be>
2024-05-22 00:10:13 +0200

    remove incorrect and obsolete calculation for weight reloading latency

--- zigzag/cost_model/cost_model_imc.py
+++ zigzag/cost_model/cost_model_imc.py
@@ -105,94 +105,15 @@ class CostModelEvaluationForIMC(CostModelEvaluation):
         super().calc_allowed_and_real_data_transfer_cycle_per_DTL()
         # Update the latency model to fit IMC requirement
         super().combine_data_transfer_rate_per_physical_port()
-        self.update_stalling_count_based_on_weight_reloading()
+        self.update_tclk()
         super().calc_data_loading_offloading_latency()
         # find the cycle count per mac
         cycles_per_mac = self.operational_array.activation_precision / self.operational_array.bit_serial_precision
         super().calc_overall_latency(cycles_per_mac=cycles_per_mac)
 
-    def update_stalling_count_based_on_weight_reloading(self):
-        """! This function calculate the stalling cycles of weight reloading for IMC (In-Memory-Computing)
-        Step 1: get port_activity_collect
-        Step 2: calculate weight reloading cycles
-        Step 3: compare with SS_comb and choose the maximum
-        Step 4: update tclk information
+    def update_tclk(self):
+        """! This function calculate the Tclk for IMC (In-Memory-Computing)
         """
-        # Step 1: calculate weight loading cycles
-        port_activity_collect: list[dict[str, list[PortActivity]]] = []
-        for mem_instance in self.mem_level_list:
-            port_activity_single: dict[str, list[PortActivity]] = {}
-            port_list = mem_instance.port_list
-            for port in port_list:
-                port_activity_single[str(port)] = []
-                for mem_op, mem_lv, mov_dir in port.served_op_lv_dir:
-                    if self.memory_operand_links.contains_mem_op(mem_op):
-                        layer_op = self.memory_operand_links.mem_to_layer_op(mem_op)
-                        period_count = self.mapping_int.unit_mem_data_movement[layer_op][
-                            mem_lv
-                        ].data_trans_period_count.get_single_dir_data(mov_dir)
-                        if period_count == 0:
-                            # skip the inactive data movement activities because they won't impact SS
-                            continue
-                        period = self.mapping_int.unit_mem_data_movement[layer_op][
-                            mem_lv
-                        ].data_trans_period.get_single_dir_data(mov_dir)
-                        real_cycle = self.real_data_trans_cycle[layer_op][mem_lv].get_single_dir_data(mov_dir)
-                        allowed_cycle = self.allowed_mem_updat_cycle[layer_op][mem_lv].get_single_dir_data(mov_dir)
-                        port_activity = PortActivity(
-                            real_cycle,
-                            allowed_cycle,
-                            period,
-                            period_count,
-                            layer_op,
-                            mem_lv,
-                            mov_dir,
-                        )
-                        port_activity_single[str(port)].append(port_activity)
-            port_activity_collect.append(port_activity_single)
-
-        # Step 2: calculate weight loading cycles
-        layer_const_operand = self.layer.constant_operands[0]  # e.g. "W"
-        # get spatial mapping in a macro
-        memory_hierarchy = self.core.mem_hierarchy_dict
-        # check if there is only one mem level for weight in accelerator. No weight loading required if that is the case.
-        weight_mem_op = self.memory_operand_links.layer_to_mem_op(layer_const_operand)
-        weight_mem_hierarchy: list[MemoryLevel] = memory_hierarchy[weight_mem_op]
-        require_weight_loading = len(weight_mem_hierarchy) != 1
-
-        # check how many times of weight reloading is required
-        # here assume imc cells is the lowest mem level for weight and rw_port
-        for _, imc_ports in port_activity_collect[0].items():  # 0: the lowest mem node in the graph
-            for port in imc_ports:
-                if port.served_op_lv_dir[2] == DataDirection.WR_IN_BY_HIGH:
-                    nb_of_weight_reload_periods = port.period_count
-
-        all_port_activity: list[PortActivity] = []
-        for imc_ports in port_activity_collect[0].values():
-            all_port_activity.extend(imc_ports)
-
-        port_activity = [
-            port_act for port_act in all_port_activity if port_act.served_op_lv_dir[2] == DataDirection.WR_IN_BY_HIGH
-        ].pop()
-        nb_of_weight_reload_periods = port_activity.period_count
-
-        # get the number of mapped rows in a macro
-        mapped_rows_total_per_macro = self.operational_array.mapped_rows_total_per_macro
-
-        # get the number of weights stored in each cell group
-        mapped_group_depth = self.operational_array.mapped_group_depth
-
-        # calculate the total number of weight loading cycles
-        if require_weight_loading:
-            weight_loading_cycles: float = nb_of_weight_reload_periods * mapped_rows_total_per_macro * mapped_group_depth
-        else:
-            weight_loading_cycles: float = 0
-        self.SS_comb_collect.append({"weight_loading_cycles": weight_loading_cycles})
-
-        # Step 3: update SS_comb
-        self.SS_comb = max(self.SS_comb, weight_loading_cycles)
-
-        # Step 4: update tclk information
         self.tclk = self.operational_array.tclk
         self.tclk_breakdown = self.operational_array.tclk_breakdown
 
