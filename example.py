107df09661c3106ba0027890b87124f00799f6f8
RobinGeens <robin.geens@kuleuven.be>
2024-07-18 10:10:57 +0200

    Tests fail, also unrelated ones? :(

--- example.py
+++ example.py
@@ -6,7 +6,7 @@ from zigzag.visualization.results.plot_cme import (
 )
 
 model = "resnet"
-workload_path = "zigzag/inputs/workload/resnet18.yaml"
+workload_path = "zigzag/inputs/workload/resnet18.yaml"  # or "zigzag/inputs/workload/resnet18.onnx"
 accelerator_path = "zigzag/inputs/hardware/tpu_like.yaml"
 mapping_path = "zigzag/inputs/mapping/tpu_like.yaml"
 pickle_filename = f"outputs/{model}-saved_list_of_cmes.pickle"
