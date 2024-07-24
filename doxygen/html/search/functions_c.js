3bcc0462f3221ed66803127457d39af70aa671b3
asyms <34397549+asyms@users.noreply.github.com>
2024-07-24 09:36:57 +0000

    Deploying to gh-pages from @ KULeuven-MICAS/zigzag@e83931bfa8b40e9dca65d6c969fb40004e47542f 🚀

--- doxygen/html/search/functions_c.js
+++ doxygen/html/search/functions_c.js
@@ -5,5 +5,5 @@ var searchData=
   ['layer_5fto_5fmem_5fop_1485',['layer_to_mem_op',['../classzigzag_1_1workload_1_1layer__attributes_1_1MemoryOperandLinks.html#ab203c91c34aeb2addccfb704a6612206',1,'zigzag::workload::layer_attributes::MemoryOperandLinks']]],
   ['limit_5flpfs_1486',['limit_lpfs',['../classzigzag_1_1opt_1_1loma_1_1LomaEngine_1_1LomaEngine.html#a5c2f2883c386fdfa25d6c7dd13da2c85',1,'zigzag::opt::loma::LomaEngine::LomaEngine']]],
   ['limit_5funrolling_5fto_5fmem_5fbandwidth_1487',['limit_unrolling_to_mem_bandwidth',['../classzigzag_1_1stages_1_1SpatialMappingGeneratorStage_1_1SpatialMappingGeneratorStage.html#a1e8c9bb99bbbcf4dccf4cd714a3b5e09',1,'zigzag::stages::SpatialMappingGeneratorStage::SpatialMappingGeneratorStage']]],
-  ['limit_5funrolling_5fto_5fmem_5fcapacity_1488',['limit_unrolling_to_mem_capacity',['../classzigzag_1_1stages_1_1SpatialMappingGeneratorStage_1_1SpatialMappingGeneratorStage.html#a7203196105656ddb799b6c11c22ddd23',1,'zigzag::stages::SpatialMappingGeneratorStage::SpatialMappingGeneratorStage']]]
+  ['limit_5funrolling_5fto_5fmem_5fcapacity_1488',['limit_unrolling_to_mem_capacity',['../classzigzag_1_1stages_1_1SpatialMappingGeneratorStage_1_1SpatialMappingGeneratorStage.html#aa7309176a2dd6d38a5a7b0e1df55b57f',1,'zigzag::stages::SpatialMappingGeneratorStage::SpatialMappingGeneratorStage']]]
 ];
