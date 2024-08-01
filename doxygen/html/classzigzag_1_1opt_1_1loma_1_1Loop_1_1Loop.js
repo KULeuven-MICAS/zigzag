b2144090e03c74f906e5bc8d0c17fa52fb814383
RobinGeens <RobinGeens@users.noreply.github.com>
2024-08-01 08:42:38 +0000

    Deploying to gh-pages from @ KULeuven-MICAS/zigzag@e084783169833c9c9306e3ac7f43ea547bf1b474 🚀

--- doxygen/html/classzigzag_1_1opt_1_1loma_1_1Loop_1_1Loop.js
+++ doxygen/html/classzigzag_1_1opt_1_1loma_1_1Loop_1_1Loop.js
@@ -1,6 +1,6 @@
 var classzigzag_1_1opt_1_1loma_1_1Loop_1_1Loop =
 [
-    [ "__init__", "classzigzag_1_1opt_1_1loma_1_1Loop_1_1Loop.html#a815dd93afaee1b63942fa1dc3e0c223e", null ],
+    [ "__init__", "classzigzag_1_1opt_1_1loma_1_1Loop_1_1Loop.html#a2288d240598c7c931bffd0583f7e371e", null ],
     [ "__repr__", "classzigzag_1_1opt_1_1loma_1_1Loop_1_1Loop.html#a9a47563093dfc5ba12274b66e368920c", null ],
     [ "__str__", "classzigzag_1_1opt_1_1loma_1_1Loop_1_1Loop.html#a23e8041ce1015febe4fdace3225714f9", null ],
     [ "layer_dim", "classzigzag_1_1opt_1_1loma_1_1Loop_1_1Loop.html#a08d71767461f55f5c3730f0371148e78", null ],
--- doxygen/html/classzigzag_1_1workload_1_1layer__attributes_1_1LayerTemporalOrdering.js
+++ doxygen/html/classzigzag_1_1workload_1_1layer__attributes_1_1LayerTemporalOrdering.js
@@ -1,7 +1,7 @@
 var classzigzag_1_1workload_1_1layer__attributes_1_1LayerTemporalOrdering =
 [
     [ "__init__", "classzigzag_1_1workload_1_1layer__attributes_1_1LayerTemporalOrdering.html#a53b9d0b3a94eee44a3a500d6e48927e0", null ],
-    [ "is_complete", "classzigzag_1_1workload_1_1layer__attributes_1_1LayerTemporalOrdering.html#a250dc5cf786dea0a556e1f1538744a01", null ],
+    [ "is_complete", "classzigzag_1_1workload_1_1layer__attributes_1_1LayerTemporalOrdering.html#ae2ca7bedfd4dee8be42e23f08ca257ac", null ],
     [ "is_empty", "classzigzag_1_1workload_1_1layer__attributes_1_1LayerTemporalOrdering.html#accdfac8ef1f0001551597b2c7d957f29", null ],
     [ "remove_invalid_layer_dims", "classzigzag_1_1workload_1_1layer__attributes_1_1LayerTemporalOrdering.html#adcfb1a2b24fcf880694f7e545829507b", null ],
     [ "to_legacy_format", "classzigzag_1_1workload_1_1layer__attributes_1_1LayerTemporalOrdering.html#a7120afd97f401d3ba2464c0c1eaddf13", null ],
