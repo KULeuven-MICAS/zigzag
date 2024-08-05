1cd383cedc9a9c422b41e0437d8714844edee5d3
RobinGeens <RobinGeens@users.noreply.github.com>
2024-08-05 16:00:47 +0000

    Deploying to gh-pages from @ KULeuven-MICAS/zigzag@5d00c8ffb9e90ce0d7a66c532bf73d2030c194d6 🚀

--- doxygen/html/classarchitecture_1_1operational__unit_1_1OperationalUnit.js
+++ doxygen/html/classarchitecture_1_1operational__unit_1_1OperationalUnit.js
@@ -1,8 +1,8 @@
 var classarchitecture_1_1operational__unit_1_1OperationalUnit =
 [
-    [ "__init__", "classarchitecture_1_1operational__unit_1_1OperationalUnit.html#a4e266b2454b341d0cd619d8d14a1500c", null ],
+    [ "__init__", "classarchitecture_1_1operational__unit_1_1OperationalUnit.html#a4baff6cee8d6668bf32e7e1a5b930eb0", null ],
     [ "__eq__", "classarchitecture_1_1operational__unit_1_1OperationalUnit.html#a4ebc07e33d57e463c0e249dfce215d96", null ],
     [ "__jsonrepr__", "classarchitecture_1_1operational__unit_1_1OperationalUnit.html#a0ab94b5e657e9550a1b27ace112c727d", null ],
     [ "area", "classarchitecture_1_1operational__unit_1_1OperationalUnit.html#a4329d2992f02e00a6e706a5443269afc", null ],
-    [ "cost", "classarchitecture_1_1operational__unit_1_1OperationalUnit.html#a6ce3360f2586a441b79b1053cfd2769c", null ]
+    [ "energy_cost", "classarchitecture_1_1operational__unit_1_1OperationalUnit.html#af8a32554ff6c62baa70316e56db314e3", null ]
 ];
