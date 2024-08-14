a36d7d2c42525bd6eaeacfa7a96d0f3fd0414524
RobinGeens <RobinGeens@users.noreply.github.com>
2024-08-14 09:36:45 +0000

    Deploying to gh-pages from @ KULeuven-MICAS/zigzag@2f281b9d7da2ba2059abcdd9fe16f9490aeeaf3b 🚀

--- doxygen/html/classzigzag_1_1parser_1_1onnx_1_1ONNXOperatorParser_1_1ONNXOperatorParser.js
+++ doxygen/html/classzigzag_1_1parser_1_1onnx_1_1ONNXOperatorParser_1_1ONNXOperatorParser.js
@@ -5,6 +5,7 @@ var classzigzag_1_1parser_1_1onnx_1_1ONNXOperatorParser_1_1ONNXOperatorParser =
     [ "get_input_output_weight_data_type", "classzigzag_1_1parser_1_1onnx_1_1ONNXOperatorParser_1_1ONNXOperatorParser.html#a41969c5f5e031c48fb8a91a78073bd28", null ],
     [ "get_intermediate_output_precision", "classzigzag_1_1parser_1_1onnx_1_1ONNXOperatorParser_1_1ONNXOperatorParser.html#adb4cffad0ef7498a2c0a0831a2d82ddd", null ],
     [ "get_node_predecessors", "classzigzag_1_1parser_1_1onnx_1_1ONNXOperatorParser_1_1ONNXOperatorParser.html#a29abb9ea8ccef05203cf5e7b3c3c0626", null ],
+    [ "get_operand_source_user_format", "classzigzag_1_1parser_1_1onnx_1_1ONNXOperatorParser_1_1ONNXOperatorParser.html#a502465f5111dfb0242c5f42d4f088e18", null ],
     [ "get_weight_name", "classzigzag_1_1parser_1_1onnx_1_1ONNXOperatorParser_1_1ONNXOperatorParser.html#a4ffc200dc5352663e26c64dc86398860", null ],
     [ "get_weight_precision", "classzigzag_1_1parser_1_1onnx_1_1ONNXOperatorParser_1_1ONNXOperatorParser.html#a2a0bce792b3642443a412af901506173", null ],
     [ "run", "classzigzag_1_1parser_1_1onnx_1_1ONNXOperatorParser_1_1ONNXOperatorParser.html#a0b93ba3298b23e01dcc349bd08b63c90", null ],
--- doxygen/html/search/variables_6.js
+++ doxygen/html/search/variables_6.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['group_5fspacing_1693',['GROUP_SPACING',['../namespacezigzag_1_1visualization_1_1results_1_1plot__cme.html#aaf17385f3d25e45640fdfb826f0a8b73',1,'zigzag::visualization::results::plot_cme']]]
+  ['group_5fspacing_1697',['GROUP_SPACING',['../namespacezigzag_1_1visualization_1_1results_1_1plot__cme.html#aaf17385f3d25e45640fdfb826f0a8b73',1,'zigzag::visualization::results::plot_cme']]]
 ];
