605bcf5d5d560726ec8cd4cdce1714b18e9c97d6
asyms <34397549+asyms@users.noreply.github.com>
2024-10-25 09:12:22 +0000

    Deploying to gh-pages from @ KULeuven-MICAS/zigzag@e6ef4a2d66fd0f72c73ce794bcb2adb039b852ba 🚀

--- doxygen/html/api_8py.js
+++ doxygen/html/api_8py.js
@@ -1,5 +1,5 @@
 var api_8py =
 [
-    [ "get_hardware_performance_zigzag", "api_8py.html#a848c2307c6f621fe6f967cce35a30206", null ],
+    [ "get_hardware_performance_zigzag", "api_8py.html#a0acbac3ee70c0e54ff94dffe4df69dc1", null ],
     [ "get_hardware_performance_zigzag_imc", "api_8py.html#a97cf011d1ddc096ee6a44473345a5865", null ]
 ];
