9d46b8c8203d289299b688c01a6707869fdd5e21
Arne Symons <arnesymons@hotmail.com>
2024-11-18 14:22:04 +0100

    update lab1 to new formats

--- lab1/main.py
+++ lab1/main.py
@@ -1,86 +1,47 @@
-import argparse
-import os
-import re
-import sys
+import logging
 
-sys.path.insert(0, os.getcwd())
-from zigzag.stages.evaluation.cost_model_evaluation import CostModelStage
-from zigzag.stages.main import MainStage
-from zigzag.stages.mapping.spatial_mapping_generation import SpatialMappingGeneratorStage
-from zigzag.stages.mapping.temporal_mapping_generator_stage import TemporalMappingGeneratorStage
-from zigzag.stages.parser.accelerator_parser import AcceleratorParserStage
-from zigzag.stages.parser.onnx_model_parser import ONNXModelParserStage
-from zigzag.stages.results.reduce_stages import MinimalLatencyStage
-from zigzag.stages.results.save import CompleteSaveStage
-from zigzag.stages.workload_iterator import WorkloadStage
+from zigzag.api import get_hardware_performance_zigzag
 from zigzag.visualization.results.plot_cme import (
     bar_plot_cost_model_evaluations_breakdown,
 )
+from zigzag.visualization.results.print_mapping import print_mapping
 
-# Get the onnx model, the mapping and accelerator arguments
-parser = argparse.ArgumentParser(description="Setup zigzag inputs")
-parser.add_argument(
-    "--model",
-    metavar="path",
-    required=False,
-    default="lab1/resnet18_first_layer.onnx",
-    help="path to onnx model, e.g. zigzag/inputs/workload/resnet18.onnx",
-)
-parser.add_argument(
-    "--mapping",
-    metavar="path",
-    required=False,
-    default="lab1/mapping.yaml",
-    help="path to mapping file, e.g., zigzag/inputs/mapping/resnet18.yaml",
-)
-parser.add_argument(
-    "--accelerator",
-    metavar="path",
-    required=False,
-    default="zigzag/inputs/hardware/tpu_like.yaml",
-    help="module path to the accelerator, e.g. zigzag/inputs/hardware/tpu_like.yaml",
-)
-args = parser.parse_args()
 
 # Initialize the logger
-import logging as _logging
+logging_level = logging.INFO
+logging_format = "%(asctime)s - %(name)s.%(funcName)s +%(lineno)s - %(levelname)s - %(message)s"
+logging.basicConfig(level=logging_level, format=logging_format)
 
-_logging_level = _logging.INFO
-# _logging_format = "%(asctime)s - %(funcName)s +%(lineno)s - %(levelname)s - %(message)s"
-_logging_format = "%(asctime)s - %(levelname)s - %(message)s"
-_logging.basicConfig(level=_logging_level, format=_logging_format)
+# Define the experiment id and pickle name
+hw_name = "tpu_like"
+workload_name = "resnet18_first_layer"
+experiment_id = f"{hw_name}-{workload_name}"
+pickle_name = f"{experiment_id}-saved_list_of_cmes"
 
-hw_name = args.accelerator.split(".")[-1]
-wl_name = re.split(r"/|\.", args.model)[-1]
-if wl_name == "onnx":
-    wl_name = re.split(r"/|\.", args.model)[-2]
-experiment_id = f"{hw_name}-{wl_name}"
+# Define main input paths
+accelerator = "zigzag/inputs/hardware/tpu_like.yaml"
+workload = "lab1/resnet18_first_layer.onnx"
+mapping = "lab1/mapping.yaml"
 
-# Initialize the MainStage which will start execution.
-# The first argument of this init is the list of stages that will be executed in sequence.
-# The second argument of this init are the arguments required for these different stages.
-mainstage = MainStage(
-    [  # Initializes the MainStage as entry point
-        ONNXModelParserStage,  # Parses the ONNX Model into the workload
-        AcceleratorParserStage,  # Parses the accelerator
-        CompleteSaveStage,  # Saves all received CMEs information to a json
-        WorkloadStage,  # Iterates through the different layers in the workload
-        SpatialMappingGeneratorStage,  # Converts the provided spatial mapping to ZigZag's internal representation
-        MinimalLatencyStage,  # Reduces all CMEs, returning minimal latency one
-        TemporalMappingGeneratorStage,  # Converts defined temporal_ordering to temporal mapping
-        CostModelStage,  # Evaluates generated SM and TM through cost model
-    ],
-    accelerator=args.accelerator,  # required by AcceleratorParserStage
-    workload=args.model,  # required by ONNXModelParserStage
-    mapping=args.mapping,  # required by ONNXModelParserStage
-    dump_folder="lab1/outputs",  # output file save pattern, ? will be replaced
-    loma_lpf_limit=6,  # required by LomaStage
-    loma_show_progress_bar=True,  # shows a progress bar while iterating over temporal mappings
+# Define other inputs of api call
+temporal_mapping_search_engine = "loma"
+optimization_criterion = "latency"
+dump_folder = f"lab1/outputs/{experiment_id}"
+pickle_filename = f"lab1/outputs/{pickle_name}.pickle"
+
+# Get the hardware performance through api call
+energy, latency, results = get_hardware_performance_zigzag(
+    accelerator=accelerator,
+    workload=workload,
+    mapping=mapping,
+    temporal_mapping_search_engine=temporal_mapping_search_engine,
+    opt=optimization_criterion,
+    dump_folder=dump_folder,
+    pickle_filename=pickle_filename,
 )
 
-# Launch the MainStage
-answers = mainstage.run()
-# Plot the energy and latency breakdown of our cost model evaluation
-cme = answers[0][0]
+# Save a bar plot of the cost model evaluations breakdown
+cmes = [result[0] for result in results[0][1]]
 save_path = "lab1/outputs/breakdown.png"
-bar_plot_cost_model_evaluations_breakdown([cme], save_path=save_path)
+bar_plot_cost_model_evaluations_breakdown(cmes, save_path=save_path)
+print_mapping(cmes[0])
