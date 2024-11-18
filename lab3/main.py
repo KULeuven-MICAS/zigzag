a1c3dfe43671945ef1813e939e0ecc296a2cbda6
Arne Symons <arnesymons@hotmail.com>
2024-11-18 15:53:41 +0100

    update lab3 to new formats

--- lab3/main.py
+++ lab3/main.py
@@ -1,17 +1,11 @@
-import os
-import sys
-
-sys.path.insert(0, os.getcwd())
-
 from zigzag.api import get_hardware_performance_zigzag
-from zigzag.visualization.results.plot_cme import bar_plot_cost_model_evaluations_total
 
 # Path to the workload onnx model
-# onnx_model_path = "zigzag/inputs/examples/workload/resnet18.onnx"
-onnx_model_path = "lab1/resnet18_first_layer.onnx"
+workload = "lab3/inputs/workload/resnet18_first_layer.onnx"
+workload_name = "resnet18_first_layer"
 
 # List of accelerators architectures we run our experiment for
-hardwares = [
+accelerators = [
     "lab3/inputs/hardware/accelerator1.yaml",
     "lab3/inputs/hardware/accelerator2.yaml",
     "lab3/inputs/hardware/accelerator3.yaml",
@@ -23,24 +17,28 @@ mappings = [
     "lab3/inputs/mapping/accelerator3.yaml",
 ]
 
-cmes = []
-for i, (hardware, mapping) in enumerate(zip(hardwares, mappings)):
-    # Pickle filename to save list of cmes
-    pickle_filename = f"lab3/outputs/list_of_cmes_{i}.pickle"
+temporal_mapping_search_engine = "loma"
+optimization_criterion = "latency"
+energies = []
+latencies = []
+for i, (accelerator, mapping) in enumerate(zip(accelerators, mappings)):
+    # Inputs for api call
+    hw_name = f"accelerator{i+1}"
+    experiment_id = f"{hw_name}-{workload_name}"
+    dump_folder = f"lab3/outputs/{experiment_id}"
+    pickle_filename = f"lab3/outputs/{experiment_id}_cmes.pickle"
     # Call the zigzag api, using a provided accelerator and mapping
     energy, latency, results = get_hardware_performance_zigzag(
-        onnx_model_path,
-        hardware,
+        workload,
+        accelerator,
         mapping,
-        opt="latency",
-        dump_filename_pattern=f"lab3/outputs/accelerator{i}.json",
+        temporal_mapping_search_engine=temporal_mapping_search_engine,
+        opt=optimization_criterion,
+        dump_folder=dump_folder,
         pickle_filename=pickle_filename,
     )
-    cmes.append(results[0][0])
+    energies.append(energy)
+    latencies.append(latency)
 
-x_labels = [f"accelerator{i}" for i in range(len(hardwares))]
-bar_plot_cost_model_evaluations_total(
-    cmes,
-    labels=x_labels,
-    save_path="lab3/outputs/plot_total.png",
-)
+for i, (energy, latency) in enumerate(zip(energies, latencies)):
+    print(f"Accelerator {i+1}: Energy =  {energy:.2e}, Latency = {latency:.2e}")
