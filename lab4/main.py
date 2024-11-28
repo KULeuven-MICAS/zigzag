d740115b8f16822c3a8e70c2c17bcfb7c7b5e625
jiacong sun <jiacong.sun@kuleuven.be>
2024-11-28 20:05:20 +0100

    update lab4 script

--- lab4/main.py
+++ lab4/main.py
@@ -3,8 +3,8 @@ import os
 import sys
 
 sys.path.insert(0, os.getcwd())
-from zigzag.hardware.architecture.ImcArray import ImcArray
-from zigzag.stages.AcceleratorParserStage import AcceleratorParserStage
+from zigzag.hardware.architecture.imc_array import ImcArray
+from zigzag.stages.parser.accelerator_parser import AcceleratorParserStage
 
 _logging_level = _logging.INFO
 _logging_format = "%(asctime)s - %(funcName)s +%(lineno)s - %(levelname)s - %(message)s"
@@ -14,12 +14,16 @@ logger = _logging.getLogger(__name__)
 hardware = "lab4/imc_macro.yaml"
 
 accelerator = AcceleratorParserStage.parse_accelerator(hardware)
-core = accelerator.get_core(1)
+core = accelerator
 imc = core.operational_array
 assert isinstance(imc, ImcArray)
+peak_energy_breakdown = imc.get_peak_energy_single_cycle()
+peak_energy = sum([v for v in imc.get_peak_energy_single_cycle().values()])
 
 logger.info("Total IMC area (mm^2): %s", round(imc.area, 4))
 logger.info("area breakdown: %s", imc.area_breakdown)
 logger.info("Tclk (ns): %s", round(imc.tclk, 4))
 logger.info("Tclk breakdown (ns): %s", imc.tclk_breakdown)
+logger.info("Peak energy (pJ/cycle): %s", round(peak_energy, 4))
+logger.info("Peak energy breakdown (pJ/cycle): %s", peak_energy_breakdown)
 exit()
